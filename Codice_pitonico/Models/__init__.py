f3a77af05d6ffe90197a92292646dd77c57d8dd9
Mastronardidavide <davide.mastronardi12@gmail.com>
2026-05-15 22:50:03 +0200

    fatto il caso d'uso gestione dispositivo

--- Codice_pitonico/Models/__init__.py
+++
@@ -1,1 +0,0 @@
-#faccio gli import
---
+++ Codice_pitonico/Services/gestore_dispositivi.py
@@ -0,0 +1,42 @@
+from Models.sensore import Sensore
+from Models.attuatore import Attuatore
+
+class GestoreDispositivi:
+    #utilizzo la Dependency Inversion: il control riceve la repository dal main
+    def __init__(self, dispositivo_repo):
+        self._dispositivo_repo = dispositivo_repo
+    # Aggiungo il caso d'uso : aggiungi dispositivo
+    def aggiungiDispositivo(self, id_disp: str, tipo: str) -> str:
+        # verifica preventiva presa fa GEstione prestiti
+        disp = self._dispositivo_repo.trovaPerID(id_disp)
+        if disp is not None:
+            return f"Errore: Dispositivo {id_disp} già presente"ù
+        
+        #creo L'entity in base al tipo richiesto
+        if tipo == "Sensore":
+            nuovo_disp = Sensore(id_disp)
+        elif tipo == "Attuatore":
+            nuovo_disp = Attuatore(id_disp)
+        else:
+            return ("Errore: Tipo dispositivo non valido")
+        
+        #aggiungo il dispositivo alla repository
+        self._dispositivo_repo.aggiungi(nuovo_disp)
+        return f"{tipo} {id_disp} aggiunto con successo"
+    #Caso d'uso rimuovi dispositivo
+    def rimuoviDispositivo(self, id_disp: str) -> str:
+        #recupero l'oggetto prima di rimuoverlo
+        disp = self._dispositivo_repo.trovaPerId(id_disp)
+        #Gestisco l'errore se non esiste
+        if disp is None:
+            return f"Errore: Dispositivo {id_disp} non trovato"
+    # rimuovo il dispositivo nella ripository
+        self._dispositivo_repo.elimina(id_disp)
+        return f"Dispositivo {id_disp} rimosso con successo"
+    
+    #Caso d'uso: visualizza dati dispositivo
+    def visualizzaDispositivo(self,id_disp: str):
+        disp = self._dispositivo_repo.trovaPerId(id_disp)
+        if disp is None:
+            return f"Errore: Dispositivo {id_disp} non trovato"
+        return disp #ritorna l'oggetto per poterlo visualizzare
