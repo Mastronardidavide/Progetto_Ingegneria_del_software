cc0ec9d642cadadb4607ffa6a6a80496e3938ea8
FedericoBattistiFAF <fede.fede.battisti05@gmail.com>
2026-05-16 17:46:55 +0200

    correzione errori indentazione zona

--- Codice_pitonico/Models/zona.py
+++ Codice_pitonico/Models/zona.py
@@ -5,39 +5,39 @@ class Zona:
         self._nome = nome
         self._orarioZona = orarioZona
 #inizializzo correttamente per evitare la condivisione della lista tra oggetti
-        self._soglieZona = sogliaZona if sogliaZona is not None else [] 
-#definisco i getter
+        self._sogliaZona = sogliaZona if sogliaZona is not None else [] 
+    #definisco i getter
 
-def getID(self) -> int: 
-    return self._id
+    def getID(self) -> int: 
+        return self._id
 
-def getNome(self) -> str:
-    return self._nome
+    def getNome(self) -> str:
+        return self._nome
 
-def getSogliaZona(self) -> list:
-    return self._sogliaZona
+    def getSogliaZona(self) -> list:
+        return self._sogliaZona
 
-def getorarioZona(self) -> datetime:
-    return self._orarioZona
-#aggiungo elemetni alla lista
-def aggiungiSoglia(self, soglia: float) -> None:
-    if not isinstance(soglia, float):
-        raise TypeError("la soglia deve essere float")
-    self._soglieZona.append(soglia)
-# definisco la serializzazione per i file json
+    def getorarioZona(self) -> datetime:
+        return self._orarioZona
+    #aggiungo elemetni alla lista
+    def aggiungiSoglia(self, soglia: float) -> None:
+        if not isinstance(soglia, float):
+            raise TypeError("la soglia deve essere float")
+        self._sogliaZona.append(soglia)
+    # definisco la serializzazione per i file json
 
-def toDict(self) -> dict:
-    return {
-        "id" : self._id,
-        "nome" : self._nome,
-        "sogliaZona" : self._sogliaZona,
-        "orarioZona" : self._orarioZona,
-    }
+    def toDict(self) -> dict:
+        return {
+            "id" : self._id,
+            "nome" : self._nome,
+            "sogliaZona" : self._sogliaZona,
+            "orarioZona" : self._orarioZona,
+        }
 
-@classmethod
-def fromDict(cls, d: dict) -> "Zona":
-    return cls(d["id"], d["nome"], d["sogliaZona"], d["orarioZona"])
+    @classmethod
+    def fromDict(cls, d: dict) -> "Zona":
+        return cls(d["id"], d["nome"], d["sogliaZona"], d["orarioZona"])
 
-#definisco il dunder per stampare le variabili, è opzionale e serve per passare un determinato valore, io lo metto per sicurezza poi da valutare se si deve togliere
-def __str__(self) -> str:
-    return f"Zona {self._id}: {self._nome} (Soglia: {self._sogliaZona})"
+    #definisco il dunder per stampare le variabili, è opzionale e serve per passare un determinato valore, io lo metto per sicurezza poi da valutare se si deve togliere
+    def __str__(self) -> str:
+        return f"Zona {self._id}: {self._nome} (Soglia: {self._sogliaZona})"
