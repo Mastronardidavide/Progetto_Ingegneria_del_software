44b7ff09b90e243e112827cc5f40cd1e7c89dca9
Mastronardidavide <davide.mastronardi12@gmail.com>
2026-05-16 16:27:40 +0200

    sensore soglio

--- Codice_pitonico/Models/sensore.py
+++ Codice_pitonico/Models/sensore.py
@@ -5,17 +5,29 @@ class Sensore(Dispositivo):
         
         super().__init__(id)
         self._nome = nome
-        self._soglia = soglia
+        #inizializzo la lista
+        self._soglia = soglia if soglia is not None else []
+    
+    
+    #def setSoglia(self, nuova_soglia: float) -> None:
+    #    self._soglia = nuova_soglia
+# ho commentato setsoglia perche secondo me si puo eliminare dato che se non facciamo aggiungi soglia
+# comunque ne settiamo una nuova.
+
+    def getSoglia(self) -> list:
+        return self._soglia
+    def aggiungiSoglia(self, soglia: float) -> None:
+        # alzo l'errore se non è float
+        if not isinstance(soglia, float):
+            raise TypeError("Ls soglia deve essere float")
+        self._soglia.append(soglia)
 
     def toDict(self) -> dict:
         return {"id": self._id, "nome": self._nome, "soglia": self._soglia}
     
-    def setSoglia(self, nuova_soglia: float) -> None:
-        self._soglia = nuova_soglia
-
-    def getSoglia(self) -> float:
-        return self._soglia
-
     @classmethod
     def fromDict(cls, d:dict) -> "Sensore":
         return cls(d["id"], d["nome"], d["soglia"])
+    #uso un dunder method per stampare i dati
+    def __str__(self) -> str:
+        return f"Sensore {self._id} (Soglie: {self._soglia})"
