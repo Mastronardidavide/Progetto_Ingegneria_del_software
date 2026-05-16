7975f4e53317f407f7ae8ea025c5d8c299afaa70
FedericoBattistiFAF <fede.fede.battisti05@gmail.com>
2026-05-16 17:59:24 +0200

    trasformato utente in classe concreta

--- Codice_pitonico/Models/utente.py
+++ Codice_pitonico/Models/utente.py
@@ -1,30 +1,22 @@
-from abc import ABC, abstractmethod
-class Autenticabile(ABC): #interfaccia
-    @abstractmethod
-    def autentica(self, utente: str, password: str) -> bool:
-        pass
-class Utente(Autenticabile): #classe astratta
+class Utente():
 
     def __init__(self, id: str, nome: str, password: str,): #aggiungo la passowrd per l'interfaccia autenticabile
         self._id = id
         self._nome = nome
         self._password = password
 
-        def getID(self) -> str:
-            return self._id
-        
-        def getNome(self) -> str:
-            return self._nome
-        
-        def toDict(self) -> dict: #per il salvataggio dei dati in Json
-            return{
-                "id": self._id,
-                "nome": self._nome,
-                "password": self._password
-            }
-        def autentica(self, utente: str, password: str) -> bool:
-            return self._nome == utente and self._password == password
+    def getID(self) -> str:
+        return self._id
+
+    def getNome(self) -> str:
+        return self._nome
+
+    def toDict(self) -> dict: #per il salvataggio dei dati in Json
+        return{
+            "id": self._id,
+            "nome": self._nome,
+            "password": self._password
+        }
+    def autentica(self, utente: str, password: str) -> bool:
+        return self._nome == utente and self._password == password #DA IMPLEMENTARE IN GESTORE UTENTI
 #non posso usare isinstance perche violiamo il principio di SOLID, Open/ Closed
-        @abstractmethod #metodo della classe astratta Utente
-        def visualizzaDatiDispositivo(self, dispositivo):
-            pass
