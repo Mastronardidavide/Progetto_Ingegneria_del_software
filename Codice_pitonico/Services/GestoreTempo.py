cf1f0897cdb320250a0434d573f9de7fe59e85c1
FedericoBattistiFAF <fede.fede.battisti05@gmail.com>
2026-05-16 19:18:00 +0200

    creato gestore tempo

---
+++ Codice_pitonico/Services/GestoreTempo.py
@@ -0,0 +1,8 @@
+#serve gestore tempo? non possiamo semplicemente far prendere l'orario agli altri gestori che ne hanno bisogno?
+
+from datetime import datetime
+from Models.Timer import Timer
+
+class GestoreTempo:
+    def __init__(self):
+        pass
