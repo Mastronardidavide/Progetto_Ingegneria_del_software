934349d8640138aa4c6aa03429de35555ff812a3
FedericoBattistiFAF <fede.fede.battisti05@gmail.com>
2026-06-07 11:18:46 +0200

    conflitti risolti?

--- Codice_pitonico/Views/timer.py
+++ Codice_pitonico/Views/timer.py
@@ -2,34 +2,26 @@ import threading
 import time
 
 class Timer:
-    def __init__(self, azione_da_eseguire, intervallo_secondi=60):
-        self._azione     = azione_da_eseguire  # Il compito da svolgere
-        self._intervallo = intervallo_secondi  # I secondi di pausa
+    def __init__(self, azione_da_eseguire, intervallo_secondi=60): #gli passo la funzione generica in modo da specificare di volta in volta e riutilizzare il codice
+        self._azione     = azione_da_eseguire  # Funzione da lanciare
+        self._intervallo = intervallo_secondi  
         self._attivo     = False
         self._thread     = None
 
     def avvia(self):
-        #Attiva il timer e lancia il thread in background
         if not self._attivo:
             self._attivo = True
-            self._thread = threading.Thread(
-                target=self._loop, 
-                daemon=True # Si chiude istantaneamente quando termini il main da terminale
-            )
+            self._thread = threading.Thread(target=self._loop, daemon=True)
             self._thread.start()
 
     def ferma(self):
-        #Disattiva il timer interrompendo in modo sicuro il ciclo continuo
         self._attivo = False
 
     def _loop(self):
-        #Ciclo continuo eseguito in parallelo dal thread secondario
         while self._attivo:
-            time.sleep(self._intervallo) # Il thread si congela per l'intervallo stabilito
-            
-            # Controllo di sicurezza prima di premere il bottone dell'azione
+            time.sleep(self._intervallo)
             if self._attivo:
                 try:
-                    self._azione() # ESEGUE L'AZIONE PASSATA (es. il backup)
+                    self._azione() # Lancia il compito assegnato
                 except Exception as e:
-                    print(f"[Timer Errore] Operazione pianificata fallita: {e}")        
+                    print(f"[Timer Errore] Operazione fallita: {e}")
